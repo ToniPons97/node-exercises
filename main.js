6691378357c75f736057367c55851e9e1353ac0e
Antonio Pons <toninopons@gmail.com>
2023-02-07 10:58:00 +0100

    Done.

---
+++ main.js
@@ -0,0 +1,10 @@
+const isEven = require('./isEven');
+
+const n = 5;
+console.log(`${n} ${isEven(n) ? 'is even.' : 'is not even.'}`);
+
+const m = 6;
+console.log(`${m} ${isEven(m) ? 'is even.' : 'is not even.'}`);
+
+const p = 7;
+console.log(`${p} ${isEven(p) ? 'is even.' : 'is not even.'}`);
