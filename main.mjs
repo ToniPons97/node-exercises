ea865efd8b8857c2163cba1b400ec3bf6996d0d1
Antonio Pons <toninopons@gmail.com>
2023-02-10 12:15:25 +0100

    First commit.

--- main.mjs
+++ main.mjs
@@ -1,20 +1,19 @@
 function luckyDraw(player) {
     return new Promise((resolve, reject) => {
-        const win = Boolean(Math.round(Math.random()));
-
-        process.nextTick(() => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
         if (win) {
-            resolve(`${player} won a prize in the draw!`);
+          resolve(`${player} won a prize in the draw!`);
         } else {
-            reject(new Error(`${player} lost the draw.`));
+          reject(new Error(`${player} lost the draw.`));
         }
-        });
+      });
     });
 }
 
-luckyDraw('Joe')
-    .then(value => console.log(value))
-    .then(luckyDraw('Caroline').then(value => console.log(value)).catch(err => console.error(err)))
-    .then(luckyDraw('Sabrina').then(value => console.log(value)).catch(err => console.error(err)))
-    .catch(err => console.log(err));
+const getResults = async (player) => luckyDraw(player).then(value => value).catch(err => err);
 
+console.log(await getResults('Tina'));
+console.log(await getResults('Jorge'));
+console.log(await getResults('Julien'));
