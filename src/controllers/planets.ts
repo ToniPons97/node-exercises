5f4bf3bedf6b00fd88f75e292d0598965b4d5be2
Antonio Pons <toninopons@gmail.com>
2023-02-12 09:35:35 +0100

    Done.

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -1,3 +1,10 @@
+import { json, Request, Response } from "express";
+
+type JsonResponse = {
+    msg: string
+}
+const jsonMessage = (msg: string): JsonResponse => ({msg});
+
 type Planet = {
     id: number,
     name: string
@@ -9,3 +16,56 @@ let planets: Planets = [
     { id: 1, name: 'Mercury' },
     { id: 2, name: 'Venus' }
 ];
+
+const isPlanetPresent = (id: string): Planet | undefined => planets.find(p => p.id === Number(id));
+
+const getAll = (req: Request, res: Response) => res.status(200).json(planets);
+const getOneById = (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const foundPlanet = isPlanetPresent(id);
+    foundPlanet ? res.status(200).json(foundPlanet) : res.status(404).json(jsonMessage('Planet not found.'));
+}
+
+const create = (req: Request, res: Response) => {
+    const { id, name } = req.body;
+
+    if (isPlanetPresent(id)) {
+        res.status(303).json(jsonMessage('Planet already exists.'));
+    } else {
+        const newPlanet: Planet = { id: Number(id), name };
+        planets = [...planets, newPlanet];
+        res.status(201).json(jsonMessage('Planet created.'));
+    }
+}
+
+const updateById = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (isPlanetPresent(id)) {
+        planets = planets.map(p => p.id === Number(id) ? {...p, name} : {...p});
+        res.status(200).json(jsonMessage('Planet updated.'));
+    } else {
+        res.status(400).json(jsonMessage('Planet not present in DB.'));
+    }
+}
+
+const deleteByID = (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    if (isPlanetPresent(id)) {
+        planets = planets.filter(p => p.id !== Number(id));
+        res.status(200).json(jsonMessage('Planet deleted.'));
+    } else {
+        res.status(400).json(jsonMessage('Planet not present in DB.'));
+    }
+}
+
+export {
+    getAll,
+    getOneById,
+    create,
+    updateById,
+    deleteByID
+}
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,12 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import morgan from 'morgan';
 import 'express-async-errors';
+import { getAll, getOneById, create, updateById, deleteByID } from './controllers/planets.js';
 
 dotenv.config();
 const port = process.env.PORT;
 
-type JsonResponse = {
-    msg: string
-}
-const jsonMessage = (msg: string): JsonResponse => ({msg});
+
 
 const app = express();
 
@@ -17,7 +15,13 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json(jsonMessage('Server is up!'));
+    res.status(200).json({ msg: 'Server is up!' });
 });
+
+app.get('/api/planets', getAll);
+app.get('/api/planets/:id', getOneById);
+app.post('/api/planets', create);
+app.put('/api/planets/:id', updateById);
+app.delete('/api/planets/:id', deleteByID);
 
 app.listen(port);
